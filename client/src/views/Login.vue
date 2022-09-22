4a582de5d1fa1349271b4f256975f0e5e23aa008
IVidackovic <gusvidiv@student.gu.se>
2022-09-22 11:23:34 +0200

    Add material to login page

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,9 +1,130 @@
 <template>
-    <div>
-        <h1>Login</h1>
+    <div id="body-content">
+        <div id="login-component">
+            <h2>Login</h2>
+            <div class="no-account-text">
+                <span>No account?</span><a href="/"><span> Create here!</span></a>
+            </div>
+
+            <div class="login-credentials">
+                <div class="login-form">
+                    <input type="text" name="email" autocomplete="off" required>
+                        <label for="email" class="email-name">
+                            <span class="content-name">email</span>
+                        </label>
+                </div>
+                <div class="login-form">
+                    <input type="password" name="password" autocomplete="off" required>
+                        <label for="password" class="password-name">
+                            <span class="content-name">password</span>
+                        </label>
+                </div>
+                <b-button class="button-login" disabled variant="primary">Disabled Login</b-button>
+            </div>
+        </div>
+
     </div>
 </template>
 
 <style>
+#body-content {
+    height: 100vh;
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    align-items: center;
+    padding: 10px;
+}
+
+#login-component {
+    background-color:white;
+    height: 70%;
+    width: 40%;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    flex-direction: column;
+    border: 1px solid black;
+}
+.no-account-text {
+    display: flex;
+    justify-content: center;
+}
+
+.no-account-text > * {
+    margin: 0.3rem;
+}
+
+.login-credentials {
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+    align-items: center;
+}
+
+.login-form {
+    width: 50%;
+    position: relative;
+    height: 50px;
+    margin: 7px;
+}
+
+.login-form input {
+    width: 100%;
+    height: 100%;
+    padding-top: 5px;
+    border: none;
+    outline: none;
+}
+.login-form label {
+    position: absolute;
+    bottom: 0px;
+    left: 0%;
+    width: 100%;
+    height: 100%;
+    pointer-events: none;
+    border-bottom: 1px solid black;
+}
+
+.login-form label::after {
+    content: "";
+    position: absolute;
+    left: 0px;
+    bottom: -1px;
+    height: 100%;
+    width: 100%;
+    transform: translateX(-100%);
+    transition: transform 0.3s ease;
+
+}
+
+.content-name {
+    position: absolute;
+    bottom: 5px;
+    left: 0px;
+    transition: all 0.3s ease;
+}
+.login-form input:focus + .email-name .content-name, .login-form input:valid + .email-name .content-name {
+    transform: translateY(-150%);
+    font-size: 14px;
+}
+
+.login-form input:focus + .email-name::after, .login-form input:valid + .email-name::after {
+    transform: translateX(0%);
+
+}
+.login-form input:focus + .password-name .content-name, .login-form input:valid + .password-name .content-name {
+    transform: translateY(-150%);
+    font-size: 14px;
+}
+
+.login-form input:focus + .password-name::after, .login-form input:valid + .password-name::after {
+    transform: translateX(0%);
+}
+
+.button-login {
+    margin-top: 3.5rem;
+}
 
 </style>
