d5a2853e4bb47f916197a7332a0b69c96632e30d
zsolnai <georg.zsolnai123@gmail.com>
2022-10-11 16:20:47 +0200

    Remove template home vue file

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 import HomeSearch from './views/HomeSearch.vue'
 import LandingPage from './views/LandingPage.vue'
 import Login from './views/Login.vue'
@@ -31,11 +30,6 @@ export default new Router({
       path: '/',
       name: 'home',
       component: HomeSearch
-    },
-    {
-      path: '/home',
-      name: 'homeNot',
-      component: Home
     },
     {
       path: '/login',
