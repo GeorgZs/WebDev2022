ffc1f6dce645007382425a1c9e46d97b853a86fe
zsolnai <georg.zsolnai123@gmail.com>
2022-10-11 16:21:03 +0200

    Add bootstrap vue columns and row to 404 page

--- client/src/views/NotFound.vue
+++ client/src/views/NotFound.vue
@@ -1,11 +1,17 @@
 <template>
     <div>
         <NavBar/>
-        <div class="not-found-container">
-            <img id="not-found-404-img" src='../images/output-onlinepngtools (2).png'/>
-            <h1>This page was not found</h1>
-            <span>How about going to the <router-link to="/">Home page</router-link> instead? &#129300;</span>
-        </div>
+        <b-row class="justify-content-md-center big-row">
+            <b-col cols="2"/>
+            <b-col cols="8">
+                <div class="not-found-container">
+                <img id="not-found-404-img" src='../images/output-onlinepngtools (2).png'/>
+                <h1>This page was not found</h1>
+                <span>How about going to the <router-link to="/">Home page</router-link> or the <router-link to="/login">Login Page</router-link> instead? &#129300;</span>
+                </div>
+            </b-col>
+            <b-col cols="2"/>
+        </b-row>
     </div>
 </template>
 
@@ -17,12 +23,16 @@ export default {
 </script>
 
 <style>
+.big-row {
+    width: 100%
+}
+
 .not-found-container {
     margin-top: 1.5rem;
 }
 
 #not-found-404-img {
-    width: 40rem;
-    height: 20rem;
+    width: 60%;
+    height: 40%;
 }
 </style>
